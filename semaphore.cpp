b01d3743a9e80f71ef9f92df7292a74637037edc
Lettergram <Lettergramm@gmail.com>
2014-05-04 12:00:55 -0500

    Semaphore example

---
+++ semaphore.cpp
@@ -0,0 +1,51 @@
+/**
+ * Semaphore example, written in C++ May 4, 2014
+ * Compiled on OSX 10.9, using:
+ * g++ -std=c++11 semaphore.cpp
+ **/
+
+#include <iostream>      
+#include <thread>        
+#include <mutex>         
+#include <condition_variable>
+ 
+std::mutex mtx;             // mutex for critical section
+std::condition_variable cv; // condition variable for critical section  
+bool ready = false;         // used to let other threads know when it is ready
+
+
+/* Prints the thread id / max number of threads */
+void print_num(int num, int max) {
+  std::unique_lock<std::mutex> lck(mtx);
+  while(!ready){ cv.wait(lck); }
+  std::cout << num + 1 << " / " << max  << std::endl;
+}
+
+/* Changes ready to true, and begins the threads printing */
+void run(){
+  std::unique_lock<std::mutex> lck(mtx);
+  ready = true;
+  cv.notify_all();
+}
+ 
+int main (){
+
+  int threadnum = 15;
+  std::thread threads[15];
+
+  /* spawn threadnum threads */
+  for (int id = 0; id < threadnum; id++)
+    threads[id] = std::thread(print_num, id, threadnum);
+
+  std::cout << "Running " << threadnum << " in parallel!" << std::endl;
+
+  run(); // Run the semaphores
+
+  /* Merge all threads to the main thread */
+  for(int id = 0; id < threadnum; id++)
+    threads[id].join();
+
+  std::cout << "Completed semaphore example!" << std::endl;
+
+  return 0;
+}
